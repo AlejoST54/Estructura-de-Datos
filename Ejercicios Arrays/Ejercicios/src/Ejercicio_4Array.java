4e0c4bda457c143b688158651168ebf5a6d7fcf4
AlejoST54 <sancheztellezjosealejandro@gmail.com>
2026-03-03 06:27:45 -0500

    hoy

---
+++ Ejercicios Arrays/Ejercicios/src/Ejercicio_4Array.java
@@ -0,0 +1,34 @@
+public class Ejercicio_4Array {
+//Control de Acceso (Gimnasio)
+//Un gimnasio tiene una lista de códigos de acceso de los socios que pagaron la mensualidad. La lista está ordenada de menor a mayor.
+
+//El Problema: Cuando un socio digita su código, el sistema debe validar si el código está en la lista de "pagos al día". Si no está, se le niega la entrada.
+//Algoritmo a usar: Búsqueda Binaria. Es eficiente para buscar códigos numéricos en una lista que ya está organizada.
+    public static void main(String[] args) {
+        int[] codigosAcceso = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
+        int codigoIngresado = 105;
+        int izquierda = 0;
+        int derecha = codigosAcceso.length - 1;
+        boolean encontrado = false;
+
+        while (izquierda <= derecha) {
+            int medio = izquierda + (derecha - izquierda) / 2;
+
+            if (codigosAcceso[medio] == codigoIngresado) {
+                encontrado = true;
+                break;
+            } else if (codigosAcceso[medio] < codigoIngresado) {
+                izquierda = medio + 1;
+            } else {
+                derecha = medio - 1;
+            }
+        }
+
+        if (encontrado) {
+            System.out.println("Acceso permitido. Bienvenido al gimnasio.");
+        } else {
+            System.out.println("Acceso denegado. Código no válido.");
+        }
+    
+    }
+}
---
+++ Ejercicios Arrays/Ejercicios/src/Ejercicio_5Array.java
@@ -0,0 +1,18 @@
+public class Ejercicio_5Array {
+//5. Contador de Estudiantes (Registro Académico)
+//Un profesor tiene un arreglo con las notas finales de 30 estudiantes (ej: 3.5, 4.0, 2.8, 5.0, ...).
+
+//El Problema: El profesor necesita saber exactamente cuántos estudiantes sacaron una nota de 5.0.
+//Algoritmo a usar: Búsqueda Lineal con Contador. Debes recorrer todo el arreglo y, cada vez que encuentres un 5.0, aumentar una variable contador.
+    public static void main(String[] args) {
+        double[] notasFinales = {3.5, 4.0, 2.8, 5.0, 4.5, 4.0, 3.0};
+        int contador=0;
+        for (int i = 0; i < notasFinales.length; i++) {
+            if (notasFinales[i] == 5.0) {
+                contador++;
+            }
+        }
+        System.out.println("Número de estudiantes con nota 5.0: " + contador);
+    }
+
+}
