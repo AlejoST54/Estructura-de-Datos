4a641fafec2016982ba40d62281b28bcd37a1693
AlejoST54 <sancheztellezjosealejandro@gmail.com>
2026-03-03 06:30:37 -0500

    .

--- Ejercicios Arrays/Ejercicios/src/Ejercicio_2Array.java
+++ Ejercicios Arrays/Ejercicios/src/Ejercicio_2Array.java
@@ -5,7 +5,7 @@ public class Ejercicio_2Array {
 //El Problema: Un cliente llega a la ventanilla y da su número. El sistema debe encontrar sus datos de la manera más rápida posible (en pocos pasos).
 //Algoritmo a usar: Búsqueda Binaria. Como los datos ya están ordenados, este algoritmo permitirá encontrar al cliente dividiendo la lista a la mitad en cada paso.
     public static void main(String[] args) {
-        int[] cedulasClientes = {1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1009, 1010};
+        int[] cedulasClientes = {1001, 1002, 1003, 10055, 10056, 10065, 10077, 10088, 10089, 10110};
         int cedulaBuscada = 1005;
         int izquierda = 0;
         int derecha = cedulasClientes.length - 1;
