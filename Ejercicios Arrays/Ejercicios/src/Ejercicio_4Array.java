81e6e461d4a947b3c1bf1dbe490d52fc13642742
AlejoST54 <sancheztellezjosealejandro@gmail.com>
2026-03-03 07:02:13 -0500

    cambio

--- Ejercicios Arrays/Ejercicios/src/Ejercicio_4Array.java
+++ Ejercicios Arrays/Ejercicios/src/Ejercicio_4Array.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Ejercicio_4Array {
 //Control de Acceso (Gimnasio)
 //Un gimnasio tiene una lista de códigos de acceso de los socios que pagaron la mensualidad. La lista está ordenada de menor a mayor.
@@ -5,8 +6,10 @@ public class Ejercicio_4Array {
 //El Problema: Cuando un socio digita su código, el sistema debe validar si el código está en la lista de "pagos al día". Si no está, se le niega la entrada.
 //Algoritmo a usar: Búsqueda Binaria. Es eficiente para buscar códigos numéricos en una lista que ya está organizada.
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         int[] codigosAcceso = {101, 102, 103, 104, 105, 106, 107, 108, 109, 110};
-        int codigoIngresado = 105;
+        System.out.println("Ingrese su codigo de acceso:");
+        int codigoIngresado = sc.nextInt();
         int izquierda = 0;
         int derecha = codigosAcceso.length - 1;
         boolean encontrado = false;
@@ -27,7 +30,7 @@ public class Ejercicio_4Array {
         if (encontrado) {
             System.out.println("Acceso permitido. Bienvenido al gimnasio.");
         } else {
-            System.out.println("Acceso denegado. Código no válido.");
+            System.out.println("Acceso denegado. No ha pagado la mensualidad.");
         }
     
     }
